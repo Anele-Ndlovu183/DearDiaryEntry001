6d2f4e43fc2743a87ddfea8951bb1b97ae4235f2
Anele Nonkululeko Ndlovu <an3l3ndlovu@gmail.com>
2025-12-04 21:13:19 +0200

    Add DearDiary.cs to express emotions

---
+++ Emotions/DearDiary.cs
@@ -0,0 +1,23 @@
+using System; 
+namespace StagesOfGrief 
+{
+  class Emotions 
+  { 
+    static void Main (string[] args) 
+    { 
+      string myName = "Penelope"; 
+      string myEmotion = "Unsure"; 
+
+      Console.WriteLine(myName + " felt " + myEmotion + " because " + myName + " was unable to completely assess the full situation on their own. "); 
+      Console.WriteLine(myName + " felt " + myEmotion +  " because " + myName + " was surrounded by overbearing and opinionated people who think they know everything, so sometimes it is hard to say no to people who think they know everything. "); 
+      Console.WriteLine(myName + " felt " + myEmotion +  " because " + myName + " doesn't know how to tell whether someone is being fake or real. "); 
+      Console.WriteLine(myName + " felt " + myEmotion + " because " + myName + " has an arrogant deadbeat father who doesn't know how to take accountability for their past transgressions. "); 
+      Console.WriteLine(myName + " felt " + myEmotion + " because " + myName + " takes a long time to assess what's going on in front of them so,they may feel lost. "); 
+    } 
+  }
+} 
+
+      
+                        
+    
+      
